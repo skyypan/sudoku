65108e930c15ebf6d9ee209599c9ac43ad438889
yudingxi <1030847081@qq.com>
2019-12-30 17:05:25 +0800

    add sudoku files

---
+++ main.h
@@ -0,0 +1,14 @@
+#include<fstream>
+#include<iostream> 
+#include<cstdio>
+#include<cstdlib>
+#include<ctime>
+#include<math.h> 
+#include<cstring>
+#include <algorithm>
+using namespace std;
+
+void intilize(int first[]);
+void Write(char * output);
+void generate_sudoku(int num);
+void solve_sudoku(string path);
